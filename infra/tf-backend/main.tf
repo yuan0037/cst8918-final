eabcef0b45465a2e9cc55746d93b406daf213798
Bo Yuan <boyuxa@syntronic.com>
2024-04-09 12:29:03 -0400

    Changed name to less than 24 characters

--- infra/tf-backend/main.tf
+++ infra/tf-backend/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "backendstate" {
-  name                     = "group6finalbackendstatesa"
+  name                     = "group6finalbestatesa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
