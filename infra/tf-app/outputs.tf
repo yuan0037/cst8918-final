f77bf1878447409629c36db0e07a625cbc04c8f7
Bo Yuan <boyuxa@syntronic.com>
2024-04-11 18:27:52 -0400

    Added acr to the infrastructure of app

--- infra/tf-app/outputs.tf
+++ infra/tf-app/outputs.tf
@@ -2,3 +2,19 @@ output "resource_group_name" {
   description = "resource group name"
   value       = azurerm_resource_group.rg.name
 }
+
+output "acr_login_server" {
+  description = "Login server for Azure Container Registry"
+  value       = azurerm_container_registry.cr.login_server
+}
+
+output "acr_username" {
+  description = "Username for Azure Container Registry"
+  value       = azurerm_container_registry.cr.admin_username
+}
+
+output "acr_password" {
+  description = "Password for Azure Container Registry"
+  value       = azurerm_container_registry.cr.admin_password
+  sensitive   = true
+}
