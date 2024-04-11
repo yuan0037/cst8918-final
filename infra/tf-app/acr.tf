b8dcf7f7d35cb7159e29edd3682ba8bdc3a0d8a2
Bo Yuan <boyuxa@syntronic.com>
2024-04-11 18:46:41 -0400

    updated format.

--- infra/tf-app/acr.tf
+++ infra/tf-app/acr.tf
@@ -1,7 +1,7 @@
 resource "azurerm_container_registry" "cr" {
-  name                     = "8918finalgroup6cr"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  sku                      = "Basic"
-  admin_enabled            = true
+  name                = "8918finalgroup6cr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
