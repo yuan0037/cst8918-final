1fae42eeefb4ab179d36a9c67a71743f8adda77a
Bo Yuan <boyuxa@syntronic.com>
2024-04-11 22:19:56 -0400

    updated redis

--- infra/tf-app/redis.tf
+++ infra/tf-app/redis.tf
@@ -2,7 +2,7 @@ resource "azurerm_redis_cache" "test_redis" {
   name                = "test-redis-yuan0037"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  capacity            = 0 # Basic tier
+  capacity            = 1 # Basic tier
   family              = "P"
   sku_name            = "Premium"
   enable_non_ssl_port = false
