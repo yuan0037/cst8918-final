8cf57cf83090d6fb4b437640db600139b8404dcc
Bo Yuan <boyuxa@syntronic.com>
2024-04-12 12:40:36 -0400

    added kube config to the output.

--- infra/tf-app/outputs.tf
+++ infra/tf-app/outputs.tf
@@ -18,3 +18,15 @@ output "acr_password" {
   value       = azurerm_container_registry.cr.admin_password
   sensitive   = true
 }
+
+output "kube_config_test" {
+  description = "Kubernetes config to connect to the cluster"
+  value       = azurerm_kubernetes_cluster.test_cluster.kube_config_raw
+  sensitive   = true
+}
+
+output "kube_config_prod" {
+  description = "Kubernetes config to connect to the prod cluster"
+  value       = azurerm_kubernetes_cluster.prod_cluster.kube_config_raw
+  sensitive   = true
+}
